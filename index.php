9be4078ea0d0a04a877a6257e85b7504aa5cb20d
Dariusz Zabrzeński <d.zabrzenski@livechatinc.com>
2018-02-06 14:06:02 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+$token = 'secret';
+
+// check token in every request
+if ($_GET['token'] !== $token) {
+    header('HTTP/1.0 401 Unauthorized');
+    exit();
+}
+
+// verification request
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    exit($_GET['challenge']);
+}
+
+header('Content-Type: application/json');
+
+$data = array(
+    'responses' => array(
+        array(
+            'type' => 'text',
+            'elements' => array('Ok, no problem')
+        )
+    )
+);
+
+echo json_encode($data);
+
+?>
