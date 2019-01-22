70a779095a051c4dac3b1aedd5eecf79629600b9
Dariusz Zabrzeński <d.zabrzenski@livechatinc.com>
2019-01-22 11:12:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $token = 'secret';
 
 // check token in every request
-if ($_GET['token'] !== $token) {
+if (!isset($_GET['token']) || $_GET['token'] !== $token) {
     header('HTTP/1.0 401 Unauthorized');
     exit();
 }
@@ -13,19 +13,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     exit($_GET['challenge']);
 }
 
-// https://stackoverflow.com/a/8945912/2378095
-// access the entity body of a POST request
-$data = json_decode(file_get_contents('php://input')); // request body
+// access the body of a POST request
+$data = json_decode(file_get_contents('php://input'));
 
 header('Content-Type: application/json');
 
-$data = array(
+$response = array(
+    'parameters' => array(
+        'name' => 'John',
+        'surname' => 'Example'
+    ),
     'responses' => array(
         array(
             'type' => 'text',
-            'elements' => array('Ok, no problem')
+            'elements' => array('Hi {{name}} {{surname}}')
         )
     )
 );
 
-echo json_encode($data);
+echo json_encode($response);
+
+?>
